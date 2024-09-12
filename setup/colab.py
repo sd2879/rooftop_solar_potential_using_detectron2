c479d4ccab2ee804d01b12c50418c648cf01a565
sd2879 <suman8deb@gmail.com>
2024-09-12 11:03:45 +0100

    v1

--- setup/colab.py
+++ setup/colab.py
@@ -1,34 +1,37 @@
 import os
 import subprocess
+from time import sleep
 import sys
 
 def install_packages():
-    # Install Python packages from requirements.txt
+    """Install Python packages from requirements.txt."""
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 
 def install_system_packages():
-    # Install system packages required for Selenium
+    """Install system packages required for Selenium and virtual display."""
     subprocess.check_call(["apt-get", "update"])
-    subprocess.check_call(["apt-get", "install", "-y", "wget", "unzip"])
-    subprocess.check_call(["wget", "https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip"])
-    subprocess.check_call(["unzip", "chromedriver_linux64.zip"])
-    subprocess.check_call(["mv", "chromedriver", "/usr/local/bin/"])
-    subprocess.check_call(["apt-get", "install", "-y", "chromium-browser"])
+    subprocess.check_call(["apt-get", "install", "-y", "xvfb"])
+    subprocess.check_call(["pip", "install", "pyvirtualdisplay", "selenium"])
 
 def setup_virtual_display():
+    """Set up a virtual display using pyvirtualdisplay."""
     from pyvirtualdisplay import Display
     display = Display(visible=0, size=(1280, 720))
     display.start()
+    sleep(5)
+    display.stop()
 
 def clone_repository():
-    # Clone the GitHub repository
-    if not os.path.exists('your_repository'):
+    """Clone the GitHub repository if it doesn't already exist."""
+    repo_dir = 'rooftop_solar_potential_using_detectron2'
+    if not os.path.exists(repo_dir):
         subprocess.check_call(["git", "clone", "https://github.com/sd2879/rooftop_solar_potential_using_detectron2.git"])
 
 def main():
+    """Main function to set up the environment and run the setup."""
     clone_repository()
-    os.chdir('your_repository')
-    install_packages()
+    os.chdir('rooftop_solar_potential_using_detectron2')
+    # install_packages()
     install_system_packages()
     setup_virtual_display()
     # Optionally, run the script here
